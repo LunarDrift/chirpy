ceb796d168ee11d5664bb8a81b91104ead2e73c9
Travis <mynameismyname1992@gmail.com>
2026-03-28 07:30:37 -0500

    File cleanup/consolidation/renaming

--- metrics.go
+++ metrics.go
@@ -15,3 +15,22 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
   </body>
 </html>`, cfg.fileserverHits.Load())
 }
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Insufficient permissions")
+		return
+	}
+
+	// delete users with sqlc query
+	err := cfg.db.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't delete users")
+		return
+	}
+
+	// reset hit count
+	cfg.fileserverHits.Store(0)
+
+	w.WriteHeader(http.StatusOK)
+}
--- tokens.go
+++ tokens.go
@@ -32,3 +32,18 @@ func (cfg *apiConfig) refreshTokenHandler(w http.ResponseWriter, r *http.Request
 
 	respondWithJSON(w, http.StatusOK, response{Token: accessToken})
 }
+
+func (cfg *apiConfig) revokeRefreshTokenHandler(w http.ResponseWriter, r *http.Request) {
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get token")
+		return
+	}
+
+	err = cfg.db.RevokeRefreshToken(r.Context(), token)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't revoke refresh token")
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
